1079ceccb4062e58978e7c62f01ac131faa0f2d7
Tomáš Bauer <bauertommy@seznam.cz>
2021-11-02 20:32:39 +0100

    comment Program.cs

--- EshopWebApi/Program.cs
+++ EshopWebApi/Program.cs
@@ -3,13 +3,22 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace EshopWebApi
 {
+    /// <summary>
+    /// Start point to application
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main method that runs
+        /// </summary>
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
+        /// <summary>
+        /// Builds web host
+        /// </summary>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
